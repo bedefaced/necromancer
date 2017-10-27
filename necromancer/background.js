f8b75ffc85a6bbb295692aa4c2fc301e1b98ea14
bedefaced <bedefaced@users.noreply.github.com>
2017-10-27 15:08:52 +0300

    Update background.js

--- necromancer/background.js
+++ necromancer/background.js
@@ -1,5 +1,5 @@
 chrome.contextMenus.create({
-            'title': 'Nectomancer encode',
+            'title': 'Necromancer encode',
             'contexts': [ 'selection' ],
             'onclick': function(info, tab) {
 				chrome.tabs.executeScript(tab.id, { file: 'content.js', frameId: info.frameId });
